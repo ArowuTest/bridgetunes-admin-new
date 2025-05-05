8c7c92336f1fe2872879bf51118fb8915e9f3e19
ArowuTest <fsanus20111@gmail.com>
2025-05-05 07:31:23 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // React 17 import
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,16 +8,15 @@ import reportWebVitals from './reportWebVitals';
 window.process = window.process || {};
 window.process.env = window.process.env || {};
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render( // React 17 API
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
